79fecf3f4e24c9e4cf9cfda1a9dce400b59f994c
Gabetn <gabriel.negash@mail.mcgill.ca>
2018-11-22 02:57:44 -0500

    init commit

---
+++ COMP251HW5/src/mancala.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+import java.math.*;
+import static java.lang.System.out;
+
+public class mancala {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
