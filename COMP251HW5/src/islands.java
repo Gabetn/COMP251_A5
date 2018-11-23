8fff304b694ad766691b30da2dbeb818f1d0440a
Gabetn <gabriel.negash@mail.mcgill.ca>
2018-11-23 05:25:52 -0500

    seemingly should work

--- COMP251HW5/src/islands.java
+++ COMP251HW5/src/islands.java
@@ -7,85 +7,206 @@ import java.math.*;
 import static java.lang.System.out;
 
 public class islands {
-	private static class Land{ //Coordinate of each land patch
-		public int x;
-		public int y;
+	private static class Graph{ //Graph //Coordinate of each land patch
+		 //key = x*y index in table and value = node #, if v=-1 not in graph
+		public HashMap<Integer, Integer> nodes;
 
-		Land(int x, int y){
-			this.x = x;
-			this.y = y;
-		}
-	}
-	private static class Isle {
-		public int level;
-		public HashSet<Land> landMass; //along a level
+		public LinkedList<Integer> adj[];
+		public int numV; //# vertices
 
-		Isle(int lvl){
-			this.level = lvl;
-			this.landMass = new HashSet<Land>();
+		Graph(int v){
+			numV = v;
+			adj = new LinkedList[numV];
+			for (int i=0; i<v; i++){ 
+				adj[i] = new LinkedList();
+			} 
 		}
+
+		public void addEdge(int u, int v){ //adds undirected edge
+			adj[u].add(v);
+			adj[v].add(u);
+		}
+
+		public int numConnectedComponents(){
+			int result = 0;
+
+			//NOTE: might need to refactor into field
+			boolean visited[] = new boolean[numV]; //init false
+			for(int i=0; i<numV; i++){
+				if(!visited[i]){
+					result++;
+					dfs(i, visited); 
+				}
+			}
+			
+
+			return result;
+		}
+
+		private void dfs(int v, boolean[] visited){
+			visited[v] = true;
+			for(Integer i : adj[v]){ //iterate through adjacency list of v
+				if(!visited[i]){
+					dfs(i,visited);
+				}
+			}
+		}
+
+		private void populateVertices(Boolean[][] t){
+			int vert=0;
+			int index=0;
+			for(int y=0; y<t.length; y++){
+				for(int x=0; x<t[0].length; x++){
+					if(t[y][x]==true){
+						nodes.put(index,vert);
+						vert++;
+					}else{
+						nodes.put(index,-1);
+					}
+					index++;
+				}
+			}
+		}
+
+
 	}
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String in = "testIslands.txt";//args[0];
-		Chart[] maps = parseInput(in); //Array of problem maps
-
+		String in = "./HW5/testIslands.txt";//args[0];
+		HashMap<Integer, Boolean[][]> tables = parseInput(in);
+		int[] numTags = numTagsPerProblem(in);
+		Graph[] maps = generateMaps(tables,numTags); //Array of problem maps
+		
+		/*
+		for(int i=0; i<tables.size(); i++){
+			Boolean[][] t = tables.get(i);
+			System.out.println(i);
+			for(int j=0; j<t.length; j++){
+				System.out.println(Arrays.toString(t[j]));
+			}
+			System.out.println('\n');
+		}
+		*/
+		
 		String out = "testIslands_solution.txt";
 		StringBuilder outputText = new StringBuilder();
-		for(Chart m : maps){ //TODO
-			m.findIslands();
-			outputText.append(m.islands.size());
+		for(Graph m : maps){ //TODO
+			int numIslands = m.numConnectedComponents();
+			outputText.append(numIslands);
 			outputText.append('\n');
 
 		}
 		//writeOutput(out,outputText.toString().trim());
 		System.out.println(outputText.toString()); //TODO remove
+
+		
 	}
 
 	public static void writeOutput(String path, String result){
 
 	}
 
-	
-	public static Chart[] parseInput(String path) throws RuntimeException{
-		Chart[] maps = null; //TODO Modify size
-
+	public static int[] numTagsPerProblem(String path){
+		int[] result=null;
 		try{
 			Scanner sc = new Scanner(new File(path));
 			String tmp = sc.nextLine();
 			int numProblems = Integer.parseInt(tmp);
-			maps = new Chart[numProblems];
+			result = new int[numProblems];
+			for(int i=0; i<numProblems; i++){
+				String[] dimensions = sc.nextLine().split("\\s+");
+				int height = Integer.parseInt(dimensions[0]);
+				
+				int numHashTag=0;
+				for(int j=0; j<height; j++){
+					tmp = sc.nextLine().trim();
+					//calculate number of hashtags per line of problem
+					numHashTag += tmp.length()-tmp.replace("#","").length();
+				}
+				result[i] = numHashTag;
+			}
+			sc.close();
+		}catch (FileNotFoundException e){
+            System.out.println("File not found!");
+            System.exit(1);
+        }
+
+		return result;
+	}
+	
+	//ToDO
+	public static Graph[] generateMaps(HashMap<Integer,Boolean[][]> tables,int[] numVertices){
+		Graph[] result = new Graph[tables.size()];
+		
+		for(int prob=0; prob<tables.size(); prob++){
+			Boolean[][] t = tables.get(prob);
+			Graph g = new Graph(numVertices[prob]); //NOTE: might be off
+			g.populateVertices(t);
+			System.out.println(numVertices[prob]); //TODO: delete
+			int currVertex=0;
+			int index=0;
+			for(int y=0; y<t.length; y++){
+				for(int x=0; x<t[0].length; x++){
+					if(t[y][x]==true){
+						//1. Check right
+						if(x<t[0].length-1 && t[y][x+1]==true){
+							int v = g.nodes.get(index+1);
+							g.addEdge(currVertex, v);
+							g.addEdge(v,currVertex); //Undirected
+						}
+						//2. Check down
+						if(y<t.length-1 && t[y+1][x]==true){
+							int v = g.nodes.get(index+t[0].length);
+							g.addEdge(currVertex, v);
+							g.addEdge(v,currVertex); //Undirected
+						}
+						//3. Increment vertex count
+						currVertex++;
+					}
+					index++;
+				}
+			}
+			result[prob] = g;
+		}
+
+
+
+		return result;
+	}
+
+
+
+	public static HashMap<Integer,Boolean[][]> parseInput(String path) throws RuntimeException{
+		HashMap<Integer, Boolean[][]> resultMap = new HashMap<>();
+		Boolean[][] map; //TODO Modify size
+		
+		try{
+			Scanner sc = new Scanner(new File(path));
+			String tmp = sc.nextLine();
+			int numProblems = Integer.parseInt(tmp);
+
 			for(int problem=0; problem<numProblems; problem++){
-				Chart c = new Chart();
 				//First line is the y and x dimensions respectively
 				String[] dimensions = sc.nextLine().split("\\s+");
 				int height = Integer.parseInt(dimensions[0]);
 				int width = Integer.parseInt(dimensions[1]); 
+				map = new Boolean[height][width];
+				
 				//Loop through all lines of data for the current problem
 				for(int y=0; y<height; y++){
 					String currLine = sc.nextLine();
-					Boolean chain = false;
-					HashSet<Isle> currLvl = new HashSet<Isle>();
-					Isle local=new Isle(y);
+
 					for(int x=0; x<width; x++){
 						char currChar = currLine.charAt(x);
 						if(currChar == '#'){ //I.e. land
-							Land l = new Land(x,y);
-							if(!chain){
-								chain=true;
-								local = new Isle(y);
-								
-							}
-							local.landMass.add(l);
-						} else if(chain){
-							chain = false;
-							currLvl.add(local);
+							map[y][x] = true;	
+						} else{
+							map[y][x] = false;
 						}
 					}
-					c.longitude.put(y, currLvl);
 				}
+				resultMap.put(problem,map);
 			}
 			sc.close();	
 		} catch (FileNotFoundException e){
@@ -93,29 +214,7 @@ public class islands {
             System.exit(1);
         }
 
-		return maps;
+		return resultMap;
 	}
-
-	private static class Chart { //Map corresponding to each individual problem
-		//For each line of the map, a set of all the connected land mass sets
-		public static HashMap<Integer, HashSet<Isle>> longitude; 
-		public static HashSet<Isle> islands;
-
-		Chart(){
-
-		}
-
-		public static void findIslands(){
-			int y = longitude.size();
-			for(int i = 1; i<y; i++){
-				HashSet<Isle> currLvl = longitude.get(i);
-				HashSet<Isle> prevLvl = 
-			}
-		}
-
-		
-	}
-
-
 
 }
